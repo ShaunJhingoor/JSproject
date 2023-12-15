a1ac34221a74e222646e9586048224014eb0384a
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-12-14 22:49:59 -0500

    Prokaryotic color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 // import Eukaryotic from "./scripts/eukaryotic"
 document.addEventListener("DOMContentLoaded", () => {
     const ctx = document.getElementById("cell1").getContext("2d")
-    
     const ctx1 = document.getElementById("cell2").getContext("2d")
     
 
@@ -17,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
    
     ctx1.beginPath();
-
+    
     ctx1.arc(225, 280 - 200 / 2, 200 / 2, Math.PI, 0);
    
     ctx1.arc(225, 280 + 200 / 2, 200 / 2, 0, Math.PI);
@@ -26,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     ctx1.fillRect(225 + 100, 275 - 80 / 2, 30, 80);
   
-    ctx1.strokeStyle = "rgb(127,246,185)";
+    ctx1.strokeStyle = "green";
     ctx1.lineWidth = 7;
 
     ctx1.closePath();
