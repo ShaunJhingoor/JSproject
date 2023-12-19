6a0525757c67428ffe5120d54fb1cbf29b1608ce
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-12-18 19:15:48 -0500

    finish eukaryotic organelles

--- src/scripts/rougher.js
+++ src/scripts/rougher.js
@@ -9,6 +9,46 @@ class Rougher{
         this.y = 277;
         this.speed = .03; 
         this.direction = 1;
+        this.modalContent = {
+            name: "Rough Endoplasmic Reticulum",
+            description: "The rough endoplasmic reticulum contains its own ribosomes. The rough endoplasmic reticulum main function is to produce proteins for the rest of the cell to function. This includes protein folding and sorting"
+        };
+        this.handleModalClick = this.handleModalClick.bind(this)
+        this.createModal();
+    }
+
+    createModal() {
+        this.modal = document.getElementById("organelleModal4");
+        this.modalName = document.getElementById("organelleName4");
+        this.modalDescription = document.getElementById("organelleDescription4");
+
+        this.modalName.textContent = this.modalContent.name;
+        this.modalDescription.textContent = this.modalContent.description;
+
+        
+        this.ctx.canvas.addEventListener("click", this.handleModalClick);
+    }
+
+    handleModalClick(event) {
+        event.stopPropagation();
+        
+        let canvas = document.getElementById("cell1");
+        let res = canvas.getBoundingClientRect();
+
+        let mouseX = event.clientX - res.x
+        let mouseY = event.clientY - res.y
+      
+                         
+        if (
+            mouseX >= this.x - 1 && mouseX <= this.x + 70 &&
+            mouseY >= this.y - 1 && mouseY <= this.y + 40
+        ) {
+            this.showModal()
+        }
+    }
+
+    showModal() {
+        this.modal.style.display = 'block';
     }
     setImageSource(imageSource){
         this.image.src = imageSource
--- src/scripts/smoother.js
+++ src/scripts/smoother.js
@@ -8,7 +8,46 @@ class Smoother{
         this.x = 200;
         this.y = 315;
         this.speed = .03; 
-        this.direction = 1;
+        this.direction = 1
+        this.modalContent = {
+            name: "Smooth Endoplasmic Reticulum",
+            description: "The smooth endoplasmic reticulum main function is to synthesize lipids, phospholids such as those in the plasma membrane and in steriods"
+        };
+        this.handleModalClick = this.handleModalClick.bind(this)
+        this.createModal();
+    }
+
+    createModal() {
+        this.modal = document.getElementById("organelleModal5");
+        this.modalName = document.getElementById("organelleName5");
+        this.modalDescription = document.getElementById("organelleDescription5");
+
+        this.modalName.textContent = this.modalContent.name;
+        this.modalDescription.textContent = this.modalContent.description;
+
+        
+        this.ctx.canvas.addEventListener("click", this.handleModalClick);
+    }
+
+    handleModalClick(event) {
+        event.stopPropagation();
+        
+        let canvas = document.getElementById("cell1");
+        let res = canvas.getBoundingClientRect();
+
+        let mouseX = event.clientX - res.x
+        let mouseY = event.clientY - res.y
+                           
+        if (
+            mouseX >= this.x - 1 && mouseX <= this.x + 40 &&
+            mouseY >= this.y - 1 && mouseY <= this.y + 40
+        ) {
+            this.showModal()
+        }
+    }
+
+    showModal() {
+        this.modal.style.display = 'block';
     }
     setImageSource(imageSource){
         this.image.src = imageSource
